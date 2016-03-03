54b9161a370201c6e26b9420db85dc2d83fe2a74
Mathew Joseph <mjoseph@qwinixtech.com>
2016-03-03 18:04:26 +0530

    aaa

--- app/src/main/java/com/qwinix/androidtest/MainActivity.java
+++ app/src/main/java/com/qwinix/androidtest/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return false;
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
